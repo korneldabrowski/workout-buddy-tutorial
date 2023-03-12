cb04a4ba46b17b23bb92fe21f3557d8acd4af424
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-13 00:32:30 +0100

    add react router dom

--- frontend/workout-buddy/src/App.tsx
+++ frontend/workout-buddy/src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1 className=" text-6xl">Workout Buddy</h1>
+
+      <BrowserRouter>
+        <div className="pages"></div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
