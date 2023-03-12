6ea6480f9063f56fa3a43b19cbe442863e37175d
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-12 19:49:52 +0100

    basic routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
+// get all workouts
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to the app." });
+});
+
+// get a Single workout
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET Single workout" });
+});
+
+// post a new workout
+router.post("/", (req, res) => {
+  res.json({ message: "POST new workout" });
+});
+
+// DELETE a workout
+router.delete("/:id", (req, res) => {
+  res.json({ message: "DELETE a workout workout" });
+});
+
+// Update the workout
+router.patch("/:id", (req, res) => {
+  res.json({ message: "UPDATE new workout" });
 });
 
 module.exports = router;
